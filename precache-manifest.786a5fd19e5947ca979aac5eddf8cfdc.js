d910f4a57326fd70fabdf26f0b4457666288bc8a
Grace Casale <gracecasale@gmail.com>
2018-11-13 20:45:28 -0500

    Updates

---
+++ precache-manifest.786a5fd19e5947ca979aac5eddf8cfdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a19f666392594a41151",
+    "url": "/boredless/static/js/runtime~main.4a19f666.js"
+  },
+  {
+    "revision": "4347ac80ec43dccfd950",
+    "url": "/boredless/static/js/main.4347ac80.chunk.js"
+  },
+  {
+    "revision": "15ae1d2a8f5d1f319d52",
+    "url": "/boredless/static/js/1.15ae1d2a.chunk.js"
+  },
+  {
+    "revision": "4347ac80ec43dccfd950",
+    "url": "/boredless/static/css/main.f9b26451.chunk.css"
+  },
+  {
+    "revision": "15ae1d2a8f5d1f319d52",
+    "url": "/boredless/static/css/1.bf01e38b.chunk.css"
+  },
+  {
+    "revision": "ddac291bb00ac1df38bbba5475f6f049",
+    "url": "/boredless/index.html"
+  }
+];
